e70212166a4440f0614d04938b04bd6204caba87
sinsoledad <3033056936@qq.com>
2025-09-30 15:46:10 +0800

    feat(interactive): 实现互动服务与仓储层
    - 新增互动仓储接口及缓存实现
    - 实现阅读数、点赞数、收藏数的增减操作
    - 支持批量增加阅读数功能- 提供获取互动数据及判断用户是否点赞/收藏逻辑- 新增互动服务接口及实现，封装业务逻辑
    - 支持通过ID列表批量获取互动数据并构建映射关系

---
+++ internal/service/interactive.go
@@ -0,0 +1,66 @@
+package service
+
+import (
+	"archi/internal/domain"
+	"archi/internal/repository"
+	"context"
+	"golang.org/x/sync/errgroup"
+)
+
+//go:generate mockgen -source=./interactive.go -package=mocks -destination=./mocks/interactive.mock.go InteractiveService
+type InteractiveService interface {
+	IncrReadCnt(ctx context.Context, biz string, bizId int64) error
+	Like(c context.Context, biz string, id int64, uid int64) error
+	CancelLike(c context.Context, biz string, id int64, uid int64) error
+	Collect(ctx context.Context, biz string, bizId, cid, uid int64) error
+	Get(ctx context.Context, biz string, id int64, uid int64) (domain.Interactive, error)
+	GetByIds(ctx context.Context, biz string, ids []int64) (map[int64]domain.Interactive, error)
+}
+type interactiveService struct {
+	repo repository.InteractiveRepository
+}
+
+func (i *interactiveService) IncrReadCnt(ctx context.Context, biz string, bizId int64) error {
+	return i.repo.IncrReadCnt(ctx, biz, bizId)
+}
+func (i *interactiveService) Like(c context.Context, biz string, id int64, uid int64) error {
+	return i.repo.IncrLike(c, biz, id, uid)
+}
+
+func (i *interactiveService) CancelLike(c context.Context, biz string, id int64, uid int64) error {
+	return i.repo.DecrLike(c, biz, id, uid)
+}
+func (i *interactiveService) Collect(ctx context.Context, biz string, bizId, cid, uid int64) error {
+	return i.repo.AddCollectionItem(ctx, biz, bizId, cid, uid)
+}
+func (i *interactiveService) Get(ctx context.Context, biz string, id int64, uid int64) (domain.Interactive, error) {
+	intr, err := i.repo.Get(ctx, biz, id)
+	if err != nil {
+		return domain.Interactive{}, err
+	}
+	var eg errgroup.Group
+	eg.Go(func() error {
+		var er error
+		intr.Liked, er = i.repo.Liked(ctx, biz, id, uid)
+		return er
+	})
+	eg.Go(func() error {
+		var er error
+		intr.Collected, er = i.repo.Collected(ctx, biz, id, uid)
+		return er
+	})
+	return intr, eg.Wait()
+}
+
+// GetByIds 用于排行榜的，所以不需要是否点赞和收藏字段
+func (i *interactiveService) GetByIds(ctx context.Context, biz string, ids []int64) (map[int64]domain.Interactive, error) {
+	intrs, err := i.repo.GetByIds(ctx, biz, ids)
+	if err != nil {
+		return nil, err
+	}
+	res := make(map[int64]domain.Interactive, len(intrs))
+	for _, intr := range intrs {
+		res[intr.BizId] = intr
+	}
+	return res, nil
+}
