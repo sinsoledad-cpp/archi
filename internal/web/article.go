8c89234e78a87859594803f5cdf3217a156db572
sinsoledad <3033056936@qq.com>
2025-09-30 16:23:47 +0800

    feat(article): 实现文章详情与互动功能
    - 修改文章错误码常量值-重命名服务字段 ArtSvc 并添加互动服务 interSvc
    - 新增获取发布文章详情接口 PubDetail，支持并发查询文章与互动数据- 新增文章点赞/取消点赞接口 Like
    - 新增文章收藏/取消收藏接口 Collect
    - 更新所有调用原 svc 方法为 ArtSvc 方法
    - 引入 errgroup 包用于并发控制

--- internal/web/article.go
+++ internal/web/article.go
@@ -10,17 +10,17 @@ import (
 	"fmt"
 	"github.com/ecodeclub/ekit/slice"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
 	"net/http"
 	"strconv"
 	"time"
 )
 
 type ArticleHandler struct {
-	svc service.ArticleService //art
-	// TODO 互动模块
-	//intrSvc service.InteractiveService
-	l   logger.Logger
-	biz string
+	ArtSvc   service.ArticleService //art
+	interSvc service.InteractiveService
+	l        logger.Logger
+	biz      string
 }
 type ArticleEditReq struct {
 	ID      int64
@@ -30,7 +30,7 @@ type ArticleEditReq struct {
 
 // Edit 接收 Article 输入，返回一个 ID，文章的 ID
 func (h *ArticleHandler) Edit(ctx *gin.Context, req ArticleEditReq, uc jwt.UserClaims) (ginx.Result, error) {
-	id, err := h.svc.Save(ctx, domain.Article{
+	id, err := h.ArtSvc.Save(ctx, domain.Article{
 		ID:      req.ID,
 		Title:   req.Title,
 		Content: req.Content,
@@ -68,7 +68,7 @@ func (h *ArticleHandler) Publish(ctx *gin.Context,
 	//	})
 	//	return
 	//}
-	id, err := h.svc.Publish(ctx, domain.Article{
+	id, err := h.ArtSvc.Publish(ctx, domain.Article{
 		ID:      req.ID,
 		Title:   req.Title,
 		Content: req.Content,
@@ -94,7 +94,7 @@ type ArticleWithdrawReq struct {
 }
 
 func (h *ArticleHandler) Withdraw(ctx *gin.Context, req ArticleWithdrawReq, uc jwt.UserClaims) (ginx.Result, error) {
-	err := h.svc.Withdraw(ctx, uc.Uid, req.Id)
+	err := h.ArtSvc.Withdraw(ctx, uc.Uid, req.Id)
 	if err != nil {
 		return ginx.Result{
 			Code: errs.ArticleInternalServerError,
@@ -137,7 +137,7 @@ func (h *ArticleHandler) Detail(ctx *gin.Context) {
 		h.l.Warn("查询文章失败，id 格式不对", logger.String("id", idstr), logger.Error(err))
 		return
 	}
-	art, err := h.svc.GetById(ctx, id)
+	art, err := h.ArtSvc.GetById(ctx, id)
 	if err != nil {
 		ctx.JSON(http.StatusOK, ginx.Result{
 			Code: errs.UserInvalidInput,
@@ -188,7 +188,7 @@ func (h *ArticleHandler) List(ctx *gin.Context) {
 	}
 	// 我要不要检测一下？
 	uc := ctx.MustGet("user").(jwt.UserClaims)
-	arts, err := h.svc.GetByAuthor(ctx, uc.Uid, page.Offset, page.Limit)
+	arts, err := h.ArtSvc.GetByAuthor(ctx, uc.Uid, page.Offset, page.Limit)
 	if err != nil {
 		ctx.JSON(http.StatusOK, ginx.Result{
 			Code: errs.ArticleInternalServerError,
@@ -217,4 +217,139 @@ func (h *ArticleHandler) List(ctx *gin.Context) {
 			}
 		}),
 	})
+}
+
+func (h *ArticleHandler) PubDetail(ctx *gin.Context) {
+	idstr := ctx.Param("id")
+	id, err := strconv.ParseInt(idstr, 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusOK, ginx.Result{
+			Code: errs.ArticleInvalidInput,
+			Msg:  "id 参数错误",
+		})
+		h.l.Warn("查询文章失败，id 格式不对",
+			logger.String("id", idstr),
+			logger.Error(err))
+		return
+	}
+
+	var (
+		eg   errgroup.Group
+		art  domain.Article
+		intr domain.Interactive
+	)
+
+	uc := ctx.MustGet("user").(jwt.UserClaims)
+	eg.Go(func() error {
+		var er error
+		art, er = h.ArtSvc.GetPubById(ctx, id, uc.Uid)
+		return er
+	})
+	eg.Go(func() error {
+		var er error
+		intr, er = h.interSvc.Get(ctx, h.biz, id, uc.Uid)
+		return er
+	})
+
+	// 等待结果
+	err = eg.Wait()
+	if err != nil {
+		ctx.JSON(http.StatusOK, ginx.Result{
+			Code: errs.ArticleInternalServerError,
+			Msg:  "系统错误",
+		})
+		h.l.Error("查询文章失败，系统错误",
+			logger.Int64("aid", id),
+			logger.Int64("uid", uc.Uid),
+			logger.Error(err))
+		return
+	}
+
+	//go func() {
+	// 1. 如果你想摆脱原本主链路的超时控制，你就创建一个新的
+	// 2. 如果你不想，你就用 ctx
+	//newCtx, cancel := context.WithTimeout(context.Background(), time.Second)
+	//defer cancel()
+	//er := h.interSvc.IncrReadCnt(newCtx, h.biz, art.Id)
+	//if er != nil {
+	//	h.l.Error("更新阅读数失败",
+	//		logger.Int64("aid", art.Id),
+	//		logger.Error(err))
+	//}
+	//}()
+
+	ctx.JSON(http.StatusOK, ginx.Result{
+		Code: http.StatusOK,
+		Msg:  "查询成功",
+		Data: ArticleVo{
+			ID:    art.ID,
+			Title: art.Title,
+
+			Content:    art.Content,
+			AuthorId:   art.Author.ID,
+			AuthorName: art.Author.Name,
+			ReadCnt:    intr.ReadCnt,
+			CollectCnt: intr.CollectCnt,
+			LikeCnt:    intr.LikeCnt,
+			Liked:      intr.Liked,
+			Collected:  intr.Collected,
+
+			Status: art.Status.ToUint8(),
+			Ctime:  art.Ctime.Format(time.DateTime),
+			Utime:  art.Utime.Format(time.DateTime),
+		},
+	})
+}
+
+type ArticleLikeReq struct {
+	Id   int64 `json:"id"`
+	Like bool  `json:"like"` // true 是点赞，false 是不点赞
+}
+
+func (h *ArticleHandler) Like(c *gin.Context, req ArticleLikeReq, uc jwt.UserClaims) (ginx.Result, error) {
+	var err error
+	if req.Like {
+		// 点赞
+		err = h.interSvc.Like(c, h.biz, req.Id, uc.Uid)
+	} else {
+		// 取消点赞
+		err = h.interSvc.CancelLike(c, h.biz, req.Id, uc.Uid)
+	}
+	if err != nil {
+		return ginx.Result{
+			Code: errs.ArticleInternalServerError,
+			Msg:  "系统错误",
+		}, err
+	}
+	return ginx.Result{
+		Code: http.StatusOK,
+		Msg:  "喜欢/取消喜欢成功",
+	}, nil
+}
+
+type ArticleCollectReq struct {
+	Id      int64 `json:"id"`
+	Cid     int64 `json:"cid"`
+	Collect bool  `json:"collect"`
+}
+
+func (h *ArticleHandler) Collect(ctx *gin.Context, req ArticleCollectReq, uc jwt.UserClaims) (ginx.Result, error) {
+	var err error
+	if req.Collect {
+		// 点赞
+		err = h.interSvc.Collect(ctx, h.biz, req.Id, req.Cid, uc.Uid)
+	} else {
+		// 取消点赞
+		err = h.interSvc.Collect(ctx, h.biz, req.Id, req.Cid, uc.Uid)
+	}
+	if err != nil {
+		return ginx.Result{
+			Code: errs.ArticleInternalServerError,
+			Msg:  "系统错误",
+		}, err
+	}
+	return ginx.Result{
+		Code: http.StatusOK,
+		Msg:  "收藏/取消收藏成功",
+	}, nil
 }
--- internal/web/errs/article.go
+++ internal/web/errs/article.go
@@ -2,7 +2,7 @@ package errs
 
 const (
 	// ArticleInvalidInput 这是一个非常含糊的错误码，代表用户相关的API参数不对
-	ArticleInvalidInput = 402001
+	ArticleInvalidInput = 403001
 	// ArticleInternalServerError 这是一个非常含糊的错误码。代表系统内部错误
-	ArticleInternalServerError = 502001
+	ArticleInternalServerError = 503001
 )
