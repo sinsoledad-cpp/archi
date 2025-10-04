5a85f58d1cd5249930802c86c7f5b52639d6dd1c
sinsoledad <3033056936@qq.com>
2025-10-04 16:42:11 +0800

    feat(reward): 新增打赏详情接口- 新增 RewardHandler 结构体及注册路由逻辑 - 实现 GetReward 方法处理打赏详情请求- 定义 GetRewardReq 请求结构体- 集成 JWT 中间件进行用户身份验证- 返回打赏状态信息并定义相关错误码

---
+++ internal/web/errs/reward.go
@@ -0,0 +1,8 @@
+package errs
+
+const (
+	// RewardInvalidInput 这是一个非常含糊的错误码，代表用户相关的API参数不对
+	RewardInvalidInput = 404001
+	// RewardInternalServerError 这是一个非常含糊的错误码。代表系统内部错误
+	RewardInternalServerError = 504001
+)
---
+++ internal/web/reward.go
@@ -0,0 +1,39 @@
+package web
+
+import (
+	"archi/internal/service"
+	"archi/internal/web/errs"
+	jwtware "archi/internal/web/middleware/jwt"
+	"archi/pkg/ginx"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type RewardHandler struct {
+	rewardSvc  service.RewardService
+	articleSvc service.ArticleService
+}
+
+func (h *RewardHandler) RegisterRoutes(server *gin.Engine) {
+	rg := server.Group("/reward")
+	rg.POST("/detail", ginx.WrapBodyAndClaims[GetRewardReq](h.GetReward))
+}
+
+type GetRewardReq struct {
+	Rid int64
+}
+
+func (h *RewardHandler) GetReward(ctx *gin.Context, req GetRewardReq, uc jwtware.UserClaims) (ginx.Result, error) {
+	resp, err := h.rewardSvc.GetReward(ctx, req.Rid, uc.Uid)
+	if err != nil {
+		return ginx.Result{
+			Code: errs.RewardInternalServerError,
+			Msg:  "系统错误",
+		}, err
+	}
+	return ginx.Result{
+		Code: http.StatusOK,
+		Msg:  "获取打赏信息成功",
+		Data: resp.Status, // 暂时也就是只需要状态
+	}, nil
+}
